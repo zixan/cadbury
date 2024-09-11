03647e7bc16a770fddc396f10e1202c9f165b1cf
Zeeshan Mughal <zeeshan.jp@gmail.com>
2024-09-11 02:09:44 -0400

    Add qb automation code and integrate with Chrome tab switching

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const NETSUITE_URL = "https://www.bbc.com/";
-export const QUICKBOOKS_URL = "https://www.cnn.com/";
+export const NETSUITE_URL = "https://www.foxnews.com/";
+export const QUICKBOOKS_URL = "https://qbo.intuit.com/";
 export const CADBURY_URL = "http://localhost:3000/";
